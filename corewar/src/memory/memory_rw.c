e656d70d2c02191b2f85c9cc9385e4374c4c44f3
Maslyn Truman <matruman@vo-j3.21-school.ru>
2020-01-13 17:46:17 +0300

    fixes

--- corewar/src/memory/memory_rw.c
+++ corewar/src/memory/memory_rw.c
@@ -35,6 +35,7 @@ void	memory_read(char *area, int pos, void *dst, int size)
 	char	*cdst;
 
 	cdst = (char *)dst;
+	i = 0;
 	while (i < size)
 	{
 		cdst[i] = area[(pos + i) % MEM_SIZE];
@@ -48,26 +49,10 @@ void	memory_write(char *area, int pos, void *src, int size)
 	char	*csrc;
 
 	csrc = (char *)src;
+	i = 0;
 	while (i < size)
 	{
 		area[(pos + i) % MEM_SIZE] = csrc[i];
 		i++;
-	}
-}
-
-void	rev_endian(void *val, int size)
-{
-	char	*p;
-	char	tmp;
-	int		i;
-
-	p = (char *)val;
-	i = 0;
-	while (i < size / 2)
-	{
-		tmp = p[size - i - 1];
-		p[size - i - 1] = p[i];
-		p[i] = tmp;
-		i++; 
 	}
 }
