b8b24758b1c5bb377141edb053c493ac60378927
Maslyn Truman <matruman@vo-i4.21-school.ru>
2020-01-15 20:21:52 +0300

    some functions

--- corewar/src/instructions/functions/inst_6_8.c
+++ corewar/src/instructions/functions/inst_6_8.c
@@ -41,9 +41,33 @@ void	and_reg(t_cursor *cursor, char *area, char *mem)
 		cursor->carry = !cursor->reg[mem[3] - 1];
 }
 
-void	and_dir(t_cursor *cursor, char *area, char *m)
+void	and_dir(t_cursor *cursor, char *area, char *mem)
 {
-	
+	if (cursor->types[1] == T_REG_CODE)
+		{
+			if (mem[1] > 16 || mem[2] > 16 || !mem[1] || !mem[2])
+				return ;
+			cursor->reg[mem[2] - 1] = 
+			cursor->reg[mem[0] - 1] & cursor->reg[mem[1] - 1];
+			cursor->carry = !cursor->reg[mem[1] - 1];
+			return;
+		}
+		if (cursor->types[1] == T_DIR_CODE)
+		{
+			if (mem[4] > 16 || !mem[4])
+				return;
+			cursor->reg[mem[5] - 1] = *(int *)(mem + 1) &
+			cursor->reg[mem[0] - 1];
+			cursor->carry = !cursor->reg[mem[5] - 1];
+			return;
+		}
+		if (mem[3] > 16 || !mem[3])
+			return;
+		memory_read(area, cursor->pos + *(short *)(mem + 1) / IDX_MOD,
+		mem + 4, 4);
+		cursor->reg[mem[3] - 1] = *(int *)(mem + 4) &
+		cursor->reg[mem[0] - 1];
+		cursor->carry = !cursor->reg[mem[3] - 1];
 }
 
 void    and(t_main *main, t_cursor *cursor, char *area)
